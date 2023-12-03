3f2834f69ccdf426e1ae6aeefdcb692d641d2e8e
Ansh Chaturvedi <ansh.chaturvedi+github@thescore.com>
2023-12-03 12:52:41 -0500

    finished part 2

--- day-3/sol.py
+++ day-3/sol.py
@@ -92,8 +92,29 @@ def solution():
 
   return ans
 
-def solution2():
-  schematic = read_file("input_large.txt")
+# --------------------------------- PART 2 ---------------------------------
+
+def maybe_get_gear_ratio_beside_part(points_to_numbers, x, y, rows, cols):
+  unique_nums = set()
+  if x-1 >= 0 and x-1 < rows:
+    if y-1 >= 0 and y-1 < cols and (x-1, y-1) in points_to_numbers: unique_nums.add(points_to_numbers[(x-1, y-1)])
+    if y >= 0 and y < cols and (x-1, y) in points_to_numbers: unique_nums.add(points_to_numbers[(x-1, y)])
+    if y+1 >= 0 and y+1 < cols and (x-1, y+1) in points_to_numbers: unique_nums.add(points_to_numbers[(x-1, y+1)])
+
+  if y-1 >= 0 and y-1 < cols and (x, y-1) in points_to_numbers: unique_nums.add(points_to_numbers[(x, y-1)])
+  if y+1 >= 0 and y+1 < cols and (x, y+1)  in points_to_numbers: unique_nums.add(points_to_numbers[(x, y+1)])
+
+  if x+1 >= 0 and x+1 < rows:
+    if y-1 >= 0 and y-1 < cols and (x+1, y-1) in points_to_numbers: unique_nums.add(points_to_numbers[(x+1, y-1)])
+    if y >= 0 and y < cols and (x+1, y) in points_to_numbers: unique_nums.add(points_to_numbers[(x+1, y)])
+    if y+1 >= 0 and y+1 < cols and (x+1, y+1) in points_to_numbers: unique_nums.add(points_to_numbers[(x+1, y+1)])
+  
+  if len(unique_nums) == 2:
+    return unique_nums.pop() * unique_nums.pop()
+  return 0
+
+def solution2(filename):
+  schematic = read_file(filename)
   rows = len(schematic)
   cols = len(schematic[0])
 
@@ -107,48 +128,45 @@ def solution2():
       if point != BLANK and point.isdigit():
         if is_valid_part(schematic, rows, cols, x, y): adj_to_parts.add((x, y))
 
-  # get sum of non-parts
-  non_parts = []
+  # get all numbers
+  numbers_to_points = {}
   for x in range(rows):
     y = 0
     while y < cols:
       point = schematic[x][y]
       if point.isdigit():
-        acc, good = [], True
-        while point.isdigit() and y < cols:
-          if (x, y) in adj_to_parts: good = False
-          point = schematic[x][y]
+        acc, points = [], []
+        while point.isdigit():
           acc.append(point)
+          points.append((x, y))
           y += 1
-        if good: non_parts.append(acc)
+          if y >= cols: break
+          point = schematic[x][y]
+
+        num = int("".join(acc))
+        if num in numbers_to_points:
+          numbers_to_points[num].append(points)
+        else:
+          numbers_to_points[num] = [points]
+          
       else:
         y += 1
 
-  # get sum of everything
-  everything = []
-  for x in range(rows):
-    y = 0
-    while y < cols:
-      point = schematic[x][y]
-      if point.isdigit():
-        acc = []
-        while point.isdigit() and y < cols:
-          point = schematic[x][y]
-          acc.append(point)
-          y += 1
-        everything.append(acc)
-      else:
-        y += 1
+  points_to_numbers = {}
+  for key, points_list in numbers_to_points.items():
+    for points in points_list:
+      for point in points:
+        points_to_numbers[point] = key
 
   ans = 0
-  for num in everything:
-    if not num[-1].isdigit(): num.pop()
-    ans += int("".join(num))
-  for num in non_parts:
-    if not num[-1].isdigit(): num.pop()
-    ans -= int("".join(num))
+  for x in range(rows):
+    for y in range(cols):
+      point = schematic[x][y]
+      if point == '*':
+        ans += maybe_get_gear_ratio_beside_part(points_to_numbers, x, y, rows, cols)
 
   return ans
 
 if __name__ == "__main__":
-  print(solution())
+  filename = "input_large.txt"
+  print(solution2(filename))
