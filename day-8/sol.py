93db8383f22d23ef216927843466acb7a4a3f0c6
Ansh Chaturvedi <ansh.chaturvedi+github@thescore.com>
2023-12-08 09:39:21 -0500

    finish part 2

--- day-8/sol.py
+++ day-8/sol.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 import re
+import math
 
 def read_file(filename):
   buffer = []
@@ -32,24 +33,41 @@ def create_graph(nodes):
     graph[start] = end
   return graph
 
+def get_count_for_node(graph, instructions, cur_node):
+  cur_instr, count = 0, 0
+  is_not_end = True
+
+  while is_not_end:
+    not_end_for_loop = False
+
+    if instructions[cur_instr] == 'L':
+      cur_node = graph[cur_node][0]
+      if cur_node[2] != 'Z': not_end_for_loop = True
+    else:
+      cur_node = graph[cur_node][1]
+      if cur_node[2] != 'Z': not_end_for_loop = True
+    
+    count += 1
+    cur_instr += 1
+    if cur_instr >= len(instructions): cur_instr = 0
+    is_not_end = not_end_for_loop
+
+  return count
+
 def solution(filename):
   instructions, nodes = read_file(filename)
   graph = create_graph(nodes)  
   instructions = [instruction for instruction in instructions]
 
-  cur_node, cur_instr = 'AAA', 0
-  count = 0
-  while cur_node != 'ZZZ':
-    if instructions[cur_instr] == 'L':
-      cur_node = graph[cur_node][0]
-    else:
-      cur_node = graph[cur_node][1]
-    count += 1
-    cur_instr += 1
-    if cur_instr >= len(instructions):
-      cur_instr = 0
+  cur_nodes = []
+  for node in graph.keys():
+    if node[2] == 'A': cur_nodes.append(node)
 
-  return count
+  counts = []
+  for node in cur_nodes:
+    counts.append(get_count_for_node(graph, instructions, node))
+
+  return math.lcm(*counts)
 
 if __name__ == "__main__":
   small = "input_small.txt"
@@ -57,5 +75,5 @@ if __name__ == "__main__":
   large = "input_large.txt"
 
   print(solution(small))
-  print(solution(small2))
+  # print(solution(small2))
   print(solution(large))
