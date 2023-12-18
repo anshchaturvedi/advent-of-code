a8b9bb9a504b387a03ea23c6327daf9eb5f2dcf1
Ansh Chaturvedi <ansh.chaturvedi+github@thescore.com>
2023-12-18 10:34:01 -0500

    solve day 2

--- dday-18/sol.py
+++ dday-18/sol.py
@@ -1,12 +1,23 @@
 from pprint import pprint
 from collections import deque
 
-# import heapq
+
+def convert_hex(hex_num):
+    decimal_part = int(hex_num[1:6], 16)  # convert the first 5 digits to decimal
+    last_digit = hex_num[6]  # get the last digit
+    return decimal_part, last_digit
 
 
 def read_file(filename, t=False):
     lines = [line.strip().split(" ") for line in open(filename)]
-    return lines
+    instrs = []
+    mapping = {"0": "R", "1": "D", "2": "L", "3": "U"}
+
+    for _, _, hex in lines:
+        dist, dir = convert_hex(hex[1:8])
+        dir = mapping[dir]
+        instrs.append((dist, dir))
+    return instrs
 
 
 def solution(filename):
@@ -14,30 +25,23 @@ def solution(filename):
 
     directions = {"U": (-1, 0), "R": (0, 1), "D": (1, 0), "L": (0, -1)}
     x, y = 0, 0
-    visited = set()
-    visited.add((0, 0))
+    all_corners = []
+    all_corners.append((0, 0))
+    boundary_area = 0
 
-    for dir, dist, _ in instrs:
+    for dist, dir in instrs:
         dx, dy = directions[dir]
-        for i in range(int(dist)):
-            x += dx
-            y += dy
-            visited.add((x, y))
+        x += dx * dist
+        y += dy * dist
+        boundary_area += dist
+        all_corners.append((x, y))
 
-    queue = deque()
-    x, y = min(visited)
-    queue.appendleft((x + 1, y + 1))
+    interior_area = 0
+    for i in range(len(all_corners) - 1):
+        (x1, y1), (x2, y2) = all_corners[i : i + 2]
+        interior_area += x1 * y2 - x2 * y1
 
-    while queue:
-        x, y = queue.pop()
-        for dx, dy in directions.values():
-            if (x + dx, y + dy) not in visited:
-                new_point = (x+dx, y+dy)
-                queue.appendleft(new_point)
-                visited.add(new_point)
-
-    return len(visited)
-
+    return (abs(interior_area) // 2) + (boundary_area // 2) + 1
 
 if __name__ == "__main__":
     small = "input_small.txt"
