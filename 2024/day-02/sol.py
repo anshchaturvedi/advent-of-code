99fc9fe464fe110e1efd44a1376f2304afeb2d45
Ansh Chaturvedi <a4chatur@uwaterloo.ca>
2024-12-02 08:19:07 -0500

    finished day 1 part 2

--- 2024/day-02/sol.py
+++ 2024/day-02/sol.py
@@ -1,4 +1,4 @@
-def solution(file_name: str):
+def solution_part_1(file_name: str):
 	input = []
 
 	with open(file_name) as input_file:
@@ -17,5 +17,36 @@ def solution(file_name: str):
 			ans += 1
 	return ans
 
-print(solution("sample.txt"))
-print(solution("full.txt"))
+def solution_part_2(file_name: str):
+	input = []
+
+	with open(file_name) as input_file:
+		line = input_file.readline()
+		while line:
+			input.append(list(map(int,line.strip().split())))
+			line = input_file.readline()
+
+	ans = 0
+	for report in input:
+		increasing = all(report[i] < report[i+1] and report[i+1] - report[i] <= 3 for i in range(len(report) - 1))
+		decreasing = all(report[i] > report[i+1] and report[i] - report[i+1] <= 3 for i in range(len(report) - 1))
+		
+		if increasing or decreasing: ans += 1
+		else:
+			for i in range(len(report)):
+				new_report = [x for x in report]
+				new_report.pop(i)
+				new_increasing = all(new_report[i] < new_report[i+1] and new_report[i+1] - new_report[i] <= 3 for i in range(len(new_report) - 1))
+				new_decreasing = all(new_report[i] > new_report[i+1] and new_report[i] - new_report[i+1] <= 3 for i in range(len(new_report) - 1))
+
+				if new_increasing or new_decreasing:
+					ans += 1
+					break
+
+	return ans
+
+print(solution_part_1("sample.txt"))
+print(solution_part_1("full.txt"))
+
+print(solution_part_2("sample.txt"))
+print(solution_part_2("full.txt"))
