4ffc702fa9266bc3f5fa664dc2e9ff135c3cd47b
Ansh Chaturvedi <a4chatur@uwaterloo.ca>
2024-12-03 08:31:38 -0500

    finish day 3 part 2

--- 2024/day-03/sol.py
+++ 2024/day-03/sol.py
@@ -22,6 +22,35 @@ def part_1_solution(file_name: str):
 
 	return res
 
+def part_2_solution(file_name: str):
+	input = []
+
+	with open(file_name) as input_file:
+		line = input_file.readline()
+		while line:
+			input.append(line)
+			line = input_file.readline()
+
+	matches = []
+	for line in input:
+		match = re.findall(r'mul\([0-9]*,[0-9]*\)|don\'t\(\)|do\(\)', line)
+		matches.extend(match)
+
+	res = 0
+	on = True
+	for match in matches:
+		if match == "don't()": on = False
+		elif match == "do()": on = True
+		elif on:
+			numbers = match[4:len(match)-1]
+			num1, num2 = numbers.split(",")
+			res += (int(num1) * int(num2))
+
+	return res
+
 
 print(part_1_solution("sample.txt"))
 print(part_1_solution("full.txt"))
+
+print(part_2_solution("sample2.txt"))
+print(part_2_solution("full.txt"))
