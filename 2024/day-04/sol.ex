9aaa41511d38afd2b7a4bc15505e00d0848e00bf
Ansh Chaturvedi <a4chatur@uwaterloo.ca>
2024-12-04 16:46:58 -0500

    finish day 4 in `Elixir`

--- 2024/day-04/sol.ex
+++ 2024/day-04/sol.ex
@@ -22,24 +22,55 @@ defmodule Day03 do
     graph = graph_to_map(graph, rows, cols)
 
     for i <- 0..(rows - 1), j <- 0..(cols - 1) do
-      up = Enum.map(i..(i - 3), fn x -> Map.get(graph, {x, j}, "") end) |> Enum.join("")
-      down = Enum.map(i..(i + 3), fn x -> Map.get(graph, {x, j}, "") end) |> Enum.join("")
-      left = Enum.map(j..(j - 3), fn x -> Map.get(graph, {i, x}, "") end) |> Enum.join("")
-      right = Enum.map(j..(j + 3), fn x -> Map.get(graph, {i, x}, "") end) |> Enum.join("")
-    end
+      if Map.get(graph, {i, j}) == "X" do
+        up = Enum.map(i..(i - 3), fn x -> Map.get(graph, {x, j}, "") end) |> Enum.join("")
+        down = Enum.map(i..(i + 3), fn x -> Map.get(graph, {x, j}, "") end) |> Enum.join("")
+        left = Enum.map(j..(j - 3), fn x -> Map.get(graph, {i, x}, "") end) |> Enum.join("")
+        right = Enum.map(j..(j + 3), fn x -> Map.get(graph, {i, x}, "") end) |> Enum.join("")
 
-    1
+        up_right = Enum.map(0..3, fn d -> Map.get(graph, {i - d, j + d}, "") end) |> Enum.join("")
+        up_left = Enum.map(0..3, fn d -> Map.get(graph, {i - d, j - d}, "") end) |> Enum.join("")
+
+        down_right =
+          Enum.map(0..3, fn d -> Map.get(graph, {i + d, j + d}, "") end) |> Enum.join("")
+
+        down_left =
+          Enum.map(0..3, fn d -> Map.get(graph, {i + d, j - d}, "") end) |> Enum.join("")
+
+        all_possible = [up, down, left, right, up_right, up_left, down_right, down_left]
+        Enum.count(all_possible, &(&1 == "XMAS"))
+      end
+    end
+    |> Enum.filter(&(not is_nil(&1)))
+    |> Enum.sum()
   end
 
-  def part2(_file_name) do
-    1
+  def part2(file_name) do
+    graph = file_name |> process_input()
+    {rows, cols} = {length(graph), graph |> List.first() |> String.length()}
+    graph = graph_to_map(graph, rows, cols)
+    valid = ["MAS", "SAM"]
+
+    for i <- 0..(rows - 1), j <- 0..(cols - 1) do
+      if Map.get(graph, {i, j}) == "A" do
+        pos_diagonal =
+          Enum.map(-1..1, fn d -> Map.get(graph, {i - d, j + d}) end) |> Enum.join("")
+
+        neg_diagonal =
+          Enum.map(-1..1, fn d -> Map.get(graph, {i + d, j + d}) end) |> Enum.join("")
+
+        if Enum.member?(valid, pos_diagonal) and Enum.member?(valid, neg_diagonal), do: 1
+      end
+    end
+    |> Enum.filter(&(not is_nil(&1)))
+    |> Enum.sum()
   end
 end
 
 IO.puts("Part 1:")
 IO.puts(" " <> Integer.to_string(Day03.part1("sample.txt")))
-# IO.puts(" " <> Integer.to_string(Day03.part1("full.txt")))
+IO.puts(" " <> Integer.to_string(Day03.part1("full.txt")))
 
-# IO.puts("Part 2:")
-# IO.puts(" " <> Integer.to_string(Day03.part2("sample2.txt")))
-# IO.puts(" " <> Integer.to_string(Day03.part2("full.txt")))
+IO.puts("Part 2:")
+IO.puts(" " <> Integer.to_string(Day03.part2("sample.txt")))
+IO.puts(" " <> Integer.to_string(Day03.part2("full.txt")))
