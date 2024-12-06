61d5f8634c2a922a42d51bc5e76c9468740fd378
Ansh Chaturvedi <a4chatur@uwaterloo.ca>
2024-12-06 13:14:28 -0500

    optimizing part 2

--- 2024/day-06/sol.py
+++ 2024/day-06/sol.py
@@ -1,5 +1,5 @@
-import pprint
 import sys
+import time
 sys.setrecursionlimit(15000000)
 
 
@@ -20,7 +20,7 @@ def part_1_solution(file_name: str):
 		for j in range(cols):
 			if input[i][j] == "^": x, y = i, j
 
-	visited = set([x, y])
+	visited = set([(x, y)])
 
 	def dfs(x, y, i):
 		if x < 0 or x >= rows or y < 0 or y >= cols: return
@@ -35,10 +35,7 @@ def part_1_solution(file_name: str):
 			dfs(new_x, new_y, i)
 	
 	dfs(x, y, 0)
-	ans = 0
-	for row in input: ans += row.count("X")
-	return ans
-				
+	return len(visited)
 
 
 def part_2_solution(file_name: str):
@@ -58,7 +55,22 @@ def part_2_solution(file_name: str):
 		for j in range(cols):
 			if input[i][j] == "^": x, y = i, j
 
-	def dfs(x, y, bad_x, bad_y, i, visited):
+	full_path_trace = set([(x, y)])
+
+	def trace_initial_path(x, y, i):
+		if x < 0 or x >= rows or y < 0 or y >= cols: return
+		full_path_trace.add((x, y))
+		new_x, new_y = x + dirs[i][0], y + dirs[i][1]
+
+		if 0 <= new_x < rows and 0 <= new_y < cols and input[new_x][new_y] == "#":
+			i = (i + 1) % len(dirs)
+			trace_initial_path(x, y, i)
+		else:
+			trace_initial_path(new_x, new_y, i)
+	
+	trace_initial_path(x, y, 0)
+
+	def dfs(x, y, i, visited):
 		if x < 0 or x >= rows or y < 0 or y >= cols: return 0
 		if (x, y, i) in visited: return 1
 
@@ -67,23 +79,28 @@ def part_2_solution(file_name: str):
 
 		if 0 <= new_x < rows and 0 <= new_y < cols and input[new_x][new_y] == "#":
 			i = (i + 1) % len(dirs)
-			return dfs(x, y, bad_x, bad_y, i, visited)
-		elif new_x == bad_x and new_y == bad_y:
-			i = (i + 1) % len(dirs)
-			return dfs(x, y, bad_x, bad_y, i, visited)
+			return dfs(x, y, i, visited)
 		else:
-			return dfs(new_x, new_y, bad_x, bad_y, i, visited)
+			return dfs(new_x, new_y, i, visited)
 
 	ans = 0
-	for i in range(rows):
-		for j in range(cols):
-			if input[i][j] == ".":
-				ans += dfs(x, y, i, j, 0, set())
+	for (i, j) in list(full_path_trace):
+		if input[i][j] == ".":
+			input[i][j] = "#"
+			ans += dfs(x, y, 0, set())
+			input[i][j] = "."
 	return ans	
 
 
-print(part_1_solution("sample.txt"))
-print(part_1_solution("full.txt"))
+def time_function(func, *args):
+	start_time = time.time()
+	result = func(*args)
+	end_time = time.time()
+	input_type = "sample" if "sample" in args[0] else "full"
+	part = "part 1" if "part_1" in func.__name__ else "part 2"
+	print(f"{input_type} input {part} took {end_time - start_time:.3f} seconds and returned {result}")
 
-print(part_2_solution("sample.txt"))
-print(part_2_solution("full.txt"))
+time_function(part_1_solution, "sample.txt")
+time_function(part_1_solution, "full.txt")
+time_function(part_2_solution, "sample.txt")
+time_function(part_2_solution, "full.txt")
