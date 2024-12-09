b909781bad3238d2a535bc5ecc134d379acdb88a
Ansh Chaturvedi <a4chatur@uwaterloo.ca>
2024-12-09 10:31:55 -0500

    optimization reducing time from 3 minutes -> under 10 seconds

--- 2024/day-09/sol.py
+++ 2024/day-09/sol.py
@@ -61,34 +61,43 @@ def part_2_solution(file_name: str):
 			for _ in range(num): res.append(".")
 			free = not free
 
-	count = 0
-	for file in reversed(sorted(sizes.keys())):
-		print(f"processing {count} / {len(sizes)}")
-		count += 1
+	free_sizes = []
+	p = 0
+	inside = False
+	cur = [None, None] # start, size
+	while p < len(res):
+		if res[p] == "." and not inside:
+			inside = True
+			cur[0], cur[1] = p, 1
+		elif res[p] == "." and inside:
+			cur[1] = cur[1] + 1
+		elif res[p] != "." and inside:
+			inside = False
+			free_sizes.append(list(cur))
+			cur = [None, None]
+		p += 1
+	# print(free_sizes)
+
+	# count = 0
+	ordered = sorted(sizes.keys(), reverse=True)
+	for file in ordered:
+		# print(f"processing {count} / {len(sizes)}")
+		# count += 1
 		file_size = sizes[file]
-		free_sizes = []
-		p = 0
-		inside = False
-		cur = [None, None] # start, size
-		while p < len(res):
-			if res[p] == "." and not inside:
-				inside = True
-				cur[0], cur[1] = p, 1
-			elif res[p] == "." and inside:
-				cur[1] = cur[1] + 1
-			elif res[p] != "." and inside:
-				inside = False
-				free_sizes.append(list(cur))
-				cur = [None, None]
-			p += 1
 
 		start_index = res.index(file)
-		for x in free_sizes:
+		for free_index, x in enumerate(free_sizes):
+			if x == [-1, -1]: continue
 			if x[1] >= file_size and x[0] < start_index:
 				for i in range(start_index, start_index + file_size):
 					res[i] = "."
 				for ax in range(x[0], x[0] + file_size):
 					res[ax] = file
+				if x[1] == file_size:
+					free_sizes[free_index] = [-1, -1]
+				else:
+					x[0] += file_size
+					x[1] -= file_size
 				break
 				
 	ans = 0
