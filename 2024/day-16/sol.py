e47a1d54c7a7f584d6dac77d61b2ffebd82c8ab8
Ansh Chaturvedi <a4chatur@uwaterloo.ca>
2024-12-16 09:36:18 -0500

    small refactors to day 16 part 2

--- 2024/day-16/sol.py
+++ 2024/day-16/sol.py
@@ -16,6 +16,7 @@ from utils.get_input import get_puzzle_input
 
 sys.setrecursionlimit(15000000)
 
+
 def part_1_solution(file_name: str):
     input = []
 
@@ -25,7 +26,7 @@ def part_1_solution(file_name: str):
             a = line.strip()
             input.append([i for i in a])
             line = input_file.readline()
-    
+
     start_x, start_y = None, None
     end_x, end_y = None, None
     rows, cols = len(input), len(input[0])
@@ -36,36 +37,31 @@ def part_1_solution(file_name: str):
                 start_x, start_y = i, j
             if input[i][j] == "E":
                 end_x, end_y = i, j
-    
-    queue = [(0, start_x, start_y, "E")] # cur_score, i, j, dir
-    distances = defaultdict(lambda: float('inf'))
+
+    queue = [(0, start_x, start_y, "E")]  # cur_score, i, j, dir
+    distances = defaultdict(lambda: float("inf"))
     dirs = {"E": (0, 1), "W": (0, -1), "N": (1, 0), "S": (-1, 0)}
     distances[(start_x, start_y)] = 0
 
-    def other_dirs(dir):
-        if dir in "EW": return ["N", "S"]
-        else: return ["E", "W"]
-    
+    def possible_dirs(dir):
+        if dir in "EW":
+            return [(dir, 1), ("N", 1001), ("S", 1001)]
+        else:
+            return [(dir, 1), ("E", 1001), ("W", 1001)]
+
     while queue:
         cost, i, j, dir = heapq.heappop(queue)
         if i == end_x and j == end_y:
             return cost
-        dx, dy = dirs[dir]
-        if i + dx in range(rows) and j + dy in range(cols):
-            if input[i+dx][j+dy] != "#":
-                new_cost = cost + 1
-                if new_cost < distances[(i+dx, j+dy)]:
-                    distances[(i+dx, j+dy)] = new_cost
-                    heapq.heappush(queue, (new_cost, i+dx, j+dy, dir))
 
-        for new_dir in other_dirs(dir):
-            dx, dy = dirs[new_dir]
-            if input[i+dx][j+dy] != "#":
-                if i + dx in range(rows) and j + dy in range(cols):
-                    new_cost = cost + 1000 + 1
-                    if new_cost < distances[(i+dx, j+dy)]:
-                        distances[(i+dx, j+dy)] = new_cost
-                        heapq.heappush(queue, (new_cost, i+dx, j+dy, new_dir))
+        for next_dir, weight in possible_dirs(dir):
+            dx, dy = dirs[next_dir]
+            if i + dx in range(rows) and j + dy in range(cols):
+                if input[i + dx][j + dy] != "#":
+                    new_cost = cost + weight
+                    if new_cost <= distances[(i + dx, j + dy, next_dir)]:
+                        distances[(i + dx, j + dy, next_dir)] = new_cost
+                        heapq.heappush(queue, (new_cost, i + dx, j + dy, next_dir))
 
 
 def part_2_solution(file_name: str):
@@ -77,7 +73,7 @@ def part_2_solution(file_name: str):
             a = line.strip()
             input.append([i for i in a])
             line = input_file.readline()
-    
+
     start_x, start_y = None, None
     end_x, end_y = None, None
     rows, cols = len(input), len(input[0])
@@ -88,19 +84,21 @@ def part_2_solution(file_name: str):
                 start_x, start_y = i, j
             if input[i][j] == "E":
                 end_x, end_y = i, j
-    
-    queue = [(0, start_x, start_y, "E", [])] # cur_score, i, j, dir
-    distances = defaultdict(lambda: float('inf'))
+
+    queue = [(0, start_x, start_y, "E", [])]  # cur_score, i, j, dir, visited
+    distances = defaultdict(lambda: float("inf"))
     dirs = {"E": (0, 1), "W": (0, -1), "N": (1, 0), "S": (-1, 0)}
     distances[(start_x, start_y, "E")] = 0
 
-    best = float('inf')
+    best = float("inf")
     map_best = defaultdict(set)
 
-    def other_dirs(dir):
-        if dir in "EW": return ["N", "S"]
-        else: return ["E", "W"]
-    
+    def possible_dirs(dir):
+        if dir in "EW":
+            return [(dir, 1), ("N", 1001), ("S", 1001)]
+        else:
+            return [(dir, 1), ("E", 1001), ("W", 1001)]
+
     while queue:
         cost, i, j, dir, visited = heapq.heappop(queue)
         if i == end_x and j == end_y:
@@ -109,28 +107,20 @@ def part_2_solution(file_name: str):
                 for node in visited:
                     map_best[best].add(node)
 
-        dx, dy = dirs[dir]
-        if i + dx in range(rows) and j + dy in range(cols):
-            if input[i+dx][j+dy] != "#":
-                new_cost = cost + 1
-                if new_cost <= distances[(i+dx, j+dy, dir)]:
-                    distances[(i+dx, j+dy, dir)] = new_cost
-                    heapq.heappush(queue, (new_cost, i+dx, j+dy, dir, visited + [(i, j)]))
+        if cost > best:
+            continue
 
-        for new_dir in other_dirs(dir):
-            dx, dy = dirs[new_dir]
-            if input[i+dx][j+dy] != "#":
-                if i + dx in range(rows) and j + dy in range(cols):
-                    new_cost = cost + 1000 + 1
-                    if new_cost <= distances[(i+dx, j+dy, dir)]:
-                        distances[(i+dx, j+dy, new_dir)] = new_cost
-                        heapq.heappush(queue, (new_cost, i+dx, j+dy, new_dir, visited + [(i, j)]))
-
-    # print(best)
-    # print(len(map_best[best]))
-    # for x, y in map_best[best]:
-    #     input[x][y] = "O"
-    # for row in input: print("".join(row))
+        for next_dir, weight in possible_dirs(dir):
+            dx, dy = dirs[next_dir]
+            if i + dx in range(rows) and j + dy in range(cols):
+                if input[i + dx][j + dy] != "#":
+                    new_cost = cost + weight
+                    if new_cost <= distances[(i + dx, j + dy, next_dir)]:
+                        distances[(i + dx, j + dy, next_dir)] = new_cost
+                        heapq.heappush(
+                            queue,
+                            (new_cost, i + dx, j + dy, next_dir, visited + [(i, j)]),
+                        )
 
     map_best[best].add((end_x, end_y))
     return len(map_best[best])
