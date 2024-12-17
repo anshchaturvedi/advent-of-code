cd69a31b61d6af2c54c316e6ac714a0888ac46f5
Ansh Chaturvedi <a4chatur@uwaterloo.ca>
2024-12-16 22:13:58 -0500

    prepare day 17

---
+++ 2024/day-17/sol.py
@@ -0,0 +1,44 @@
+import argparse
+from collections import Counter, defaultdict, deque
+import heapq
+import math
+import os
+from time import sleep
+import numpy as np
+import pprint
+import re
+import sys
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+sys.path.append(ROOT_DIR)
+from utils.aoc_tools import time_function, nums
+from utils.get_input import get_puzzle_input
+
+sys.setrecursionlimit(15000000)
+
+
+def part_1_solution(file_name: str):
+    input = open(file_name).readlines()
+    input = [x.strip() for x in input]
+
+
+def part_2_solution(file_name: str):
+    input = open(file_name).readlines()
+    input = [x.strip() for x in input]
+
+
+# ---------------------------- RUN AND SUBMIT ----------------------------
+
+
+parser = argparse.ArgumentParser(description="Advent of Code Solution Script")
+parser.add_argument(
+    "-s", "--submit", action="store_true", help="Submit the solution to Advent of Code"
+)
+args = parser.parse_args()
+
+# get_puzzle_input(suppress_logs=True)
+
+time_function(False, part_1_solution, "sample.txt")
+time_function(args.submit, part_1_solution, "full.txt")
+time_function(False, part_2_solution, "sample.txt")
+time_function(args.submit, part_2_solution, "full.txt")
