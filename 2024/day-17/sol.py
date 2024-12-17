6e2f3a9cf59a7013857c2dc2afcb454fa9e4fd98
Ansh Chaturvedi <a4chatur@uwaterloo.ca>
2024-12-17 11:19:40 -0500

    solved part 2 with math and huge hint
    I realized that the solution had to do something with the last
    three bits of A since the general pattern was bitshifting by 3
    some number of times, but I couldn't come up with the definitive
    pattern. Turns out you can actually go in reverse and keep multiplying
    by 3 every time the ends of the instructions and the new program
    match, and that reduces the search space quite a bit.
    
    Subreddit came in clutch!

--- 2024/day-17/sol.py
+++ 2024/day-17/sol.py
@@ -4,6 +4,7 @@ import heapq
 import math
 import os
 from time import sleep
+import time
 import numpy as np
 import pprint
 import re
@@ -81,63 +82,67 @@ def part_2_solution(file_name: str):
     c = nums(input[2])[0]
     instrs = nums(input[4])
 
-    def get_operand(operand):
-        if operand == 4: return a
-        elif operand == 5: return b
-        elif operand == 6: return c
-        elif operand == 7: return 7
-        else: return operand
+    def simulate(start_a_value):
+        def get_operand(operand):
+            if operand == 4: return reg_a
+            elif operand == 5: return reg_b
+            elif operand == 6: return reg_c
+            elif operand == 7: return 7
+            else: return operand
 
-    maxxx = 1000000000000000000
-    for start_a in range(maxxx):
-        # if start_a % 8 != instrs[-1]: continue
-        if start_a % 10000000 == 0:
-            print(f"{100 * start_a / maxxx}%")
-        a = start_a
+        reg_a = start_a_value
+        reg_b = b
+        reg_c = c
         i = 0
         ans = []
-        counter = 0
+
         while i < len(instrs):
             op, operand = instrs[i], instrs[i+1]
             x = get_operand(operand)
             if op == 0:
-                new_a = a / (2 ** x)
-                a = int(new_a)
+                reg_a = int(reg_a / (2 ** x))
                 i += 2
             elif op == 1:
-                b = b ^ operand
+                reg_b = reg_b ^ operand
                 i += 2
             elif op == 2:
-                b = x % 8
+                reg_b = x % 8
                 i += 2
             elif op == 3:
-                if a == 0:
+                if reg_a == 0:
                     i += 2
                 else:
                     i = operand
             elif op == 4:
-                b = b ^ c
+                reg_b = reg_b ^ reg_c
                 i += 2
             elif op == 5:
                 ans.append(x % 8)
-                if instrs[counter] != ans[-1]:
-                    break
-                counter += 1
-                if counter == len(instrs):
-                    print(ans)
-                    return start_a
                 i += 2
             elif op == 6:
-                new_a = a / (2 ** x)
-                b = int(new_a)
+                new_a = reg_a / (2 ** x)
+                reg_b = int(new_a)
                 i += 2
             elif op == 7:
-                new_a = a / (2 ** x)
-                c = int(new_a)
+                new_a = reg_a / (2 ** x)
+                reg_c = int(new_a)
                 i += 2
 
-    return -1
+        return ans
 
+    possibilities = {0: [x for x in range(8)]}
+    for exponent in range(1, len(instrs)):
+        possibilities[exponent] = []
+        for p in possibilities[exponent - 1]:
+            for q in range(8):
+                if p != 0:
+                    a = 8 * p + q
+                    out = simulate(a)
+                    l = len(out)
+                    if out == instrs[len(instrs) - l:]:
+                        possibilities[exponent].append(a)
+                    if out == instrs:
+                        return a
 
 # ---------------------------- RUN AND SUBMIT ----------------------------
 
