a5a9665e7161697c7f8dfa6c635e01b325344c24
Ansh Chaturvedi <a4chatur@uwaterloo.ca>
2024-12-18 19:29:31 -0500

    prepare day 19

---
+++ 2024/day-19/sol.py
@@ -0,0 +1,33 @@
+import argparse
+from collections import Counter, defaultdict, deque
+import heapq
+import math
+import os
+from time import sleep
+import time
+import numpy as np
+import pprint
+import re
+import sys
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+sys.path.append(ROOT_DIR)
+from utils.aoc_tools import time_function, nums
+
+sys.setrecursionlimit(15000000)
+
+def part_1_solution(file_name: str):
+    input = open(file_name).readlines()
+    input = [x.strip() for x in input]
+
+def part_2_solution(file_name: str):
+    input = open(file_name).readlines()
+    input = [x.strip() for x in input]
+
+
+# ---------------------------- SUBMIT ----------------------------
+
+time_function(part_1_solution, "sample.txt")
+time_function(part_1_solution, "full.txt")
+# time_function(part_2_solution, "sample.txt")
+# time_function(part_2_solution, "full.txt")
