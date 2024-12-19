dfed3c0916f92c099acf110540af854994dc1c3e
Ansh Chaturvedi <a4chatur@uwaterloo.ca>
2024-12-19 00:26:31 -0500

    finish day 19 both parts
    I had such a stupid off-by-one error in my indexing when I
    performed my `dfs` implementation on the words, I could've
    easily leaderboarded today :( Maybe some other time!

--- 2024/day-19/sol.py
+++ 2024/day-19/sol.py
@@ -1,5 +1,6 @@
 import argparse
 from collections import Counter, defaultdict, deque
+from functools import lru_cache
 import heapq
 import math
 import os
@@ -20,14 +21,55 @@ def part_1_solution(file_name: str):
     input = open(file_name).readlines()
     input = [x.strip() for x in input]
 
+    towels = set([i.strip() for i in input[0].split(",")])
+    words = [word for word in input[2:]]
+
+    def dfs(i, w):
+        if i >= len(w):
+            return True
+
+        for j in range(i + 1, len(w) + 1):
+            substr = w[i:j]
+            if substr in towels and dfs(j, w):
+                return True
+
+        return False
+
+    ans = 0
+    for word in words:
+        if dfs(0, word):
+            ans += 1
+    return ans
+
 def part_2_solution(file_name: str):
     input = open(file_name).readlines()
     input = [x.strip() for x in input]
+
+    towels = set([i.strip() for i in input[0].split(",")])
+    words = [word for word in input[2:]]
+
+    @lru_cache
+    def dfs(i, w):
+        if i >= len(w):
+            return 1
+
+        res = 0
+        for j in range(i + 1, len(w) + 1):
+            substr = w[i:j]
+            if substr in towels:
+                res += dfs(j, w)
+
+        return res
+
+    ans = 0
+    for word in words:
+        ans += dfs(0, word)
+    return ans
 
 
 # ---------------------------- SUBMIT ----------------------------
 
 time_function(part_1_solution, "sample.txt")
 time_function(part_1_solution, "full.txt")
-# time_function(part_2_solution, "sample.txt")
-# time_function(part_2_solution, "full.txt")
+time_function(part_2_solution, "sample.txt")
+time_function(part_2_solution, "full.txt")
